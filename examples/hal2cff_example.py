7848f7bbd5ecc22420d1b222d11361aa35766d4e
yannickT <yannickT@eduroam-65-104.loria.fr>
2021-11-19 15:37:54 +0100

    tutorial step 1

--- examples/hal2cff_example.py
+++ examples/hal2cff_example.py
@@ -5,4 +5,19 @@ from hal2cff import hal2cff
 
 print(hal2cff("https://hal.archives-ouvertes.fr/hal-01361430v1"))
 
+import ipywidgets as widgets
 
+query = widgets.Textarea(value="https://hal.archives-ouvertes.fr/hal-01361430v1")
+button = widgets.Button(description="Run query")
+display(widgets.HBox([query, button]))
+output = widgets.Output()
+display(output)
+
+
+def run_and_display_query(_):
+    hal2cff(query.value)
+    with output:
+        display(hal2cff(query.value))
+
+
+button.on_click(run_and_display_query)
